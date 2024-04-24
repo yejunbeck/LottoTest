749dec507ac523fbf55c95a125679e725b2acbf1
yejunbeck <yejun110227@gmail.com>
2024-04-24 14:43:52 +0900

    project create

---
+++ src/main/kotlin/org/example/lottotest/LottoTestApplication.kt
@@ -0,0 +1,11 @@
+package org.example.lottotest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottoTestApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/lottotest/LottoTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.lottotest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
