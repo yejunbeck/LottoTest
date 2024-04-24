de6c092461229d38ced458dbc6ebe2c3a293584c
yejunbeck <yejun110227@gmail.com>
2024-04-24 15:17:52 +0900

    rotto result method add

--- src/main/kotlin/org/example/lottotest/RandomLottoApi.kt
+++ src/main/kotlin/org/example/lottotest/RandomLottoApi.kt
@@ -6,18 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class LottoApiApplication
+class RandomLottoApi
 
 fun main(args: Array<String>) {
-    runApplication<LottoApiApplication>(*args)
+    runApplication<RandomLottoApi>(*args)
 }
 
 @RestController
 class LottoController {
 
+    // 당첨 번호
+    private val winningNumbers = generateWinningNumbers()
+
     // 랜덤 로또 번호 생성 API
     @GetMapping("/lotto")
-    fun generateLottoNumbers(): List<Int> {
+    fun generateLottoNumbers(): LottoResult {
         val lottoNumbers = mutableListOf<Int>()
         val random = java.util.Random()
 
@@ -29,7 +32,34 @@ class LottoController {
             }
         }
 
-        // 생성된 로또 번호 반환
-        return lottoNumbers
+        // 당첨 번호와 비교하여 당첨 여부 확인
+        val matchedNumbers = lottoNumbers.intersect(winningNumbers).toList()
+        val isWinner = matchedNumbers.size >= 3
+
+        // 결과 반환
+        return LottoResult(lottoNumbers, winningNumbers, matchedNumbers, isWinner)
+    }
+
+    // 당첨 번호 생성 메서드
+    private fun generateWinningNumbers(): List<Int> {
+        val winningNumbers = mutableListOf<Int>()
+        val random = java.util.Random()
+
+        while (winningNumbers.size < 7) {
+            val randomNumber = random.nextInt(45) + 1
+            if (!winningNumbers.contains(randomNumber)) {
+                winningNumbers.add(randomNumber)
+            }
+        }
+
+        return winningNumbers
     }
 }
+
+// 로또 결과 클래스
+data class LottoResult(
+    val userNumbers: List<Int>,          // 사용자의 로또 번호
+    val winningNumbers: List<Int>,       // 당첨 번호
+    val matchedNumbers: List<Int>,       // 사용자의 번호와 당첨 번호의 일치하는 번호
+    val isWinner: Boolean                // 당첨 여부
+)
